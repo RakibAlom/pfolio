b9e7db90c1cd08c3fec1f27a9fefa8f69d347944
RakibAlom <rakibalom17@gmail.com>
2022-12-12 03:27:57 +0600

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/Routes/Routes";
+
 function App() {
   return (
-    <div className="App">
-      <h2>Project Running</h2>
-    </div>
+    <>
+      <RouterProvider router={router}></RouterProvider>
+    </>
   );
 }
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Header = () => {
+const Home = () => {
   return (
     <div>
-      <h2>Header</h2>
+      <h1>Home</h1>
     </div>
   );
 };
 
-export default Header;
+export default Home;
