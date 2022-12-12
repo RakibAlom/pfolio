6c9a4886c6b71e77d116013a505d6391746322c9
RakibAlom <rakibalom17@gmail.com>
2022-12-13 01:17:23 +0600

    portfolios api created

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle wares
+app.use(cors())
+app.use(express.json());
+
+const portfolios = require('./portfolios.json');
+
+app.get('/portfolios', (req, res) => {
+  res.send(portfolios);
+})
+
+app.get('/portfolio/:id', (req, res) => {
+  const portfolio = portfolios.find(portfolio => portfolio.id == req.params.id);
+  res.send(portfolio);
+})
+
+app.get('/', (req, res) => {
+  res.send('rakib alom server is running')
+})
+
+app.listen(port, () => {
+  console.log(`rakib alom server running on port: ${port}`)
+})
